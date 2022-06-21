169d1d243a68116d8ff3182881484428512f2ecd
artamonovoleg <artamonovav2003@gmail.com>
2022-06-22 01:51:40 +0300

    fix: c++ usage fix

--- sources/renderer/nuklear/ft_nuklear.h
+++ sources/renderer/nuklear/ft_nuklear.h
@@ -17,6 +17,33 @@
 extern "C"
 {
 #endif
+	NK_API struct nk_context *
+	nk_ft_init( struct ft_wsi_info *wsi_info,
+	            struct ft_device   *device,
+	            struct ft_queue    *queue,
+	            enum ft_format      color_format,
+	            enum ft_format      depth_format );
+
+	FT_API void
+	nk_ft_shutdown( void );
+	NK_API void
+	nk_ft_font_stash_begin( struct nk_font_atlas **atlas );
+	NK_API void
+	nk_ft_font_stash_end( void );
+	NK_API void
+	nk_ft_new_frame( void );
+
+	NK_API void
+	nk_ft_render( const struct ft_command_buffer *cmd,
+	              enum nk_anti_aliasing           AA );
+
+	NK_API void
+	nk_ft_device_destroy( void );
+	NK_API void
+	nk_ft_device_create( struct ft_device *,
+	                     struct ft_queue *graphics_queue,
+	                     enum ft_format,
+	                     enum ft_format );
 
 	NK_API struct nk_context *
 	nk_ft_init( struct ft_wsi_info *wsi_info,
