692225475cbd8a80d72d12e4de11944b071c9037
artamonovoleg <artamonovav2003@gmail.com>
2022-07-02 01:49:29 +0300

    remove platform dependent code from timer

--- sources/os/timer.h
+++ sources/os/timer.h
@@ -1,26 +1,21 @@
 #pragma once
 
-#include <time.h>
 #include "base/base.h"
+#include "application.h"
 
 struct ft_timer
 {
-	struct timespec start;
+	uint64_t start;
 };
 
 FT_INLINE void
 ft_timer_reset( struct ft_timer* timer )
 {
-	clock_gettime( CLOCK_MONOTONIC_RAW, &timer->start );
+	timer->start = ft_get_time();
 }
 
 FT_INLINE uint64_t
 ft_timer_get_ticks( struct ft_timer* timer )
 {
-	struct timespec now;
-	clock_gettime( CLOCK_MONOTONIC_RAW, &now );
-	return ( uint64_t ) ( ( ( int64_t ) ( now.tv_sec - timer->start.tv_sec ) *
-	                        1000 ) +
-	                      ( ( now.tv_nsec - timer->start.tv_nsec ) /
-	                        CLOCKS_PER_SEC ) );
+	return ft_get_time() - timer->start;
 }
