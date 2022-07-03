3754cf4c7174a96abda5e835766f44b4321ab6ab
artamonovoleg <artamonovav2003@gmail.com>
2022-07-03 18:55:55 +0300

    use relative include paths

--- sources/fluent/os.h
+++ sources/fluent/os.h
@@ -1,13 +1,13 @@
-#include "log/log.h"
+#include "../log/log.h"
 
-#include "base/base.h"
+#include "../base/base.h"
 
-#include "os/window.h"
-#include "os/application.h"
-#include "os/input.h"
-#include "os/camera.h"
-#include "os/timer.h"
+#include "../os/window.h"
+#include "../os/application.h"
+#include "../os/input.h"
+#include "../os/camera.h"
+#include "../os/timer.h"
 
-#include "math/linear.h"
+#include "../math/linear.h"
 
-#include "fs/fs.h"
+#include "../fs/fs.h"
--- sources/fluent/renderer.h
+++ sources/fluent/renderer.h
@@ -1,11 +1,12 @@
-#include "log/log.h"
+#include "../log/log.h"
 
-#include "wsi/wsi.h"
+#include "../wsi/wsi.h"
 
-#include "renderer/backend/renderer_enums.h"
-#include "renderer/backend/renderer_backend.h"
-#include "renderer/backend/render_graph.h"
-#include "renderer/nuklear/ft_nuklear.h"
-#include "renderer/scene/model_loader.h"
+#include "../renderer/backend/renderer_enums.h"
+#include "../renderer/backend/renderer_backend.h"
+#include "../renderer/backend/render_graph.h"
+#include "../renderer/nuklear/ft_nuklear.h"
+#include "../renderer/scene/model_loader.h"
+#include "../renderer/scene/animation.h"
 
-#include "math/linear.h"
+#include "../math/linear.h"
